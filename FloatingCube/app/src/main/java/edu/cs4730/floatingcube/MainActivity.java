98e9fddde4c2d3c4238e3ee46cb1afa5e95dbe9f
JimSeker <seker@uwyo.edu>
2015-07-16 16:46:46 -0600

    initial two examples, plus readme.

---
+++ FloatingCube/app/src/main/java/edu/cs4730/floatingcube/MainActivity.java
@@ -0,0 +1,52 @@
+package edu.cs4730.floatingcube;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
+/*
+ * This example separates the activity from the renderer so it is easier to figure what
+ * is the activity part (like the trigger) versa the renderer code.
+ */
+
+public class MainActivity extends CardboardActivity {
+
+    private Vibrator vibrator;
+    private CardboardOverlayView overlayView;
+    String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_ui);
+
+        //setup the cardbardview and set the renderer for it.
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(new myStereoRenderer());
+        setCardboardView(cardboardView);
+
+        //this is overlay code from google, that allows us to put text on the "screen" easily.
+        overlayView = (CardboardOverlayView) findViewById(R.id.overlay);
+        overlayView.show3DToast("Welcome to the demo.");
+
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    /**
+     * Called when the Cardboard trigger is pulled.
+     */
+    @Override
+    public void onCardboardTrigger() {
+        Log.i(TAG, "onCardboardTrigger");
+
+        overlayView.show3DToast("you triggered it.");
+
+        // gives the user some feedback.
+        vibrator.vibrate(50);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CardBoardVR
+These examples are for Android's CardBoard.  They are built with AndroidStudio in java.<BR>
+
+<b>CardBoardSampe</b> is Google's example code, but setup for studio correctly.<BR>
+
+<b>FloatingCube</b> is a floating cube in space.  It is based on the OpenGL30Cube example.  This example seperates the classes to make it easier to see the CardBoardActivity and the StereoRenderer.  The cube and color class are the same as the opengl30cube example.  The CardboardOverlayView is borrowed from the CardBoardSample code.<BR>
+
+These are example code for University of Wyoming, Cosc 4730 Mobile Programming course.  All examples are for Android.
